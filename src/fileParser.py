0e7d9b821e5d610bcd4f6cbfca5b72cb0a1ebac9
Justin Lui <jklui11032gmail.com>
2024-02-23 22:51:41 -0500

    added a way to check for nested parenthesis

--- src/fileParser.py
+++ src/fileParser.py
@@ -5,6 +5,14 @@ from difference import difference
 from projection import projection,selection
 from join import join
 from intersection import intersection
+def parenthetic_contents(string):
+    stack = []
+    for i, c in enumerate(string):
+        if c == '(':
+            stack.append(i)
+        elif c == ')' and stack:
+            start = stack.pop()
+            yield (string[start + 1: i])
 def main():
     '''
     fileName = input("File Name: ")
@@ -21,7 +29,7 @@ def main():
         fileName = input("File Name: ")
     '''
     df1 = pd.DataFrame({
-        'A': [1, 2, 3, 4, 5],
+        'A': [7, 2, 8, 1, 3],
         'B': ['a', 'b', 'c', 'd', 'e'],
         'C': [10.5, 20.3, 30.1, 40.7, 50.9],
         'D': [True, False, True, False, True],
@@ -29,15 +37,18 @@ def main():
     })
 
     df2 = pd.DataFrame({
-        'A': [3, 4, 5, 6],
-        'B': ['c', 'd', 'e', 'f']
+    'A': [4, 1, 5, 6, 9],
+    'B': ['x', 'y', 'z', 'w', 'v'],
+    'C': [15.2, 25.6, 35.8, 45.3, 55.1],
+    'D': [False, True, False, True, False],
+    'E': ['pineapple', 'mango', 'strawberry', 'blueberry', 'watermelon']
     })
-
-    print("Union: \n" ,union(df1,df2))
-    print("Difference: \n",difference(df1,df2))
-    print("Projection: \n",projection(df1, ['A','C','E']))
-    print("Selection: \n", selection(df1,"A > 2"))
-    print("Temp Select: \n", df1[df1['A'] > 2])
+    #print("Union: \n" ,union(df1,df2))
+    #print("Difference: \n",difference(df1,df2))
+    #print("Projection: \n",projection(df1, ['A','C','E']))
+    print(df1)
+    print("Selection: \n", selection(df1,"A > 2 or B < 'e' or E = True"))
+    #print(list(parenthetic_contents("((R x S) x T) and (V x B)")))
     #print("Cartesian: \n",join.cartesianProduct(df1,df2))
     #print("Intersection: \n",intersection(df1,df2))
     #print("Natural Join: \n",join.naturalJoin(df1,df2))
--- src/projection.py
+++ src/projection.py
@@ -10,18 +10,28 @@ def projection(dataTable1,conditions):
         return dataTableList
     else:
         return dataTable1[[conditions]]
-def selection(dataTable1,conditions):
-    pattern = r'(\S+)\s*([><=]+)\s*(\S+)'
-    matches = re.match(pattern, conditions)
-    if matches:
-        lhs = matches.group(1)  
-        op = matches.group(2)   
-        rhs = matches.group(3) 
 
-        print("Left-hand side:", lhs)
-        print("Operator:", op)
-        print("Right-hand side:", rhs)
-    else:
-        print("Invalid inequality string")
-    filtered = dataTable1[dataTable1[lhs].apply(lambda x: eval(f"x {op} {rhs}"))]
+def selection(dataTable1,conditions):
+    #print(conditions)
+    cList = re.split(r'\b( and | or )\b',conditions)
+    print("CLIST:",cList)
+    filtered  = dataTable1
+    condList = [x.strip() for x in cList if x.strip() in [' and ', ' or ']]
+    print("Cond List:",condList)
+    for condition in cList:
+        #print(condition)
+        pattern = r'(\S+)\s*([><=]+)\s*(\S+)'
+        matches = re.match(pattern, condition)
+        if matches:
+            lhs = matches.group(1)  
+            op = matches.group(2)   
+            rhs = matches.group(3) 
+
+            #print("Left-hand side:", lhs)
+            #print("Operator:", op)
+            #print("Right-hand side:", rhs)
+        else:
+            print("Invalid inequality string")
+        
+        filtered = filtered[filtered[lhs].apply(lambda x: eval(f"x {op} {rhs}"))]
     return filtered
