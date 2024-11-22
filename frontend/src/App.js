e7e7a8358d202b5e3a9ae845e4808cdd2e6d1c61
Bjourn Etienne <87133985+Bjourn0@users.noreply.github.com>
2024-11-22 17:05:18 -0500

    Create App.js
    Co-Authored-By: Hitgan0 <163173846+Hitgan0@users.noreply.github.com>

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+export default function App() {
+  return (
+    <div className = "Container"> 
+
+     <div className = "Header">
+      <h1 className = "Title">
+        REX - Relational Algebra Explorer
+      </h1>
+      <button className = "Wiki">
+        wiki🌐
+      </button>
+     </div>
+    </div>
+  );
+}    
