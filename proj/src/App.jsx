1e830a1dae907f237f6e629d97a58193c4e84202
hemanskolla <33156254+hemanskolla@users.noreply.github.com>
2024-12-10 14:28:04 -0500

    Translating Existing Work
    Locally, site is beginning to look like our work with create-react-app (CRA).
    
    Corrected:
    - File Paths
    - File Types
    - File Structure Reorganization into Vite's template structure from CRA
    - Debugging few errors on Console
    
    Pending:
    - Add metadata (meta tags and manifest.json)
    - Add Dino ASCII art
    - Other stuff

--- proj/src/App.jsx
+++ proj/src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// import dinoASCII from './dinoASCII.png';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className = "Container"> 
+
+     <div className = "Header">
+      <h1 className = "Title">
+        REX - Relational Algebra Explorer
+      </h1>
+      <button className = "Wiki">
+        wiki🌐
+      </button>
+     </div>
+      
+      <div className = "Calc">
+        <h3 className = "Relations">
+          Relations
+        </h3>
+        <textarea
+          className="Query"
+          placeholder="Type Query Here..."
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <div className = "Butt">
+        <button className = "Execute">Execute Query</button> 
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className = "cheatDino">
+        <h4 className = "Cheat">
+          <ul className = "List1">
+            <p>Cheat-Sheet: </p>
+            <li>\union -{'>'} "∨"</li>
+            <li>\intersect -{'>'} "∧"</li>
+            <li>\select -{'>'} "σ"</li>
+            <li>\project -{'>'} "π"</li>
+            <li>\cart -{'>'} "⨯"</li>
+          </ul>
+          <ul className = "List2">
+            <li>\join -{'>'} “⨝”</li>
+            <li>{'<'}= -{'>'}“≤”</li>         
+            <li>{'>'}= -{'>'}“≥”</li>  
+            <li>!= -{'>'}“≠”</li>               
+            </ul>
+        </h4>
+        {/* <h5 className = "Dino">
+                            
+        </h5> */}
+        {/* <img src={dinoASCII} alt="Dino"  style={{ width: '225px', height: '125px' }}/> */}
+      </div>
+    </div>
   )
 }
 
