a1f7364171195673aa2c61b7240de75df464cb1f
hemanskolla <33156254+hemanskolla@users.noreply.github.com>
2024-12-10 11:59:35 -0500

    Fix Babel & @jridgewell plugin/module Installation
    Error Message:
    plugin:vite:react-babel] [BABEL] /mnt/c/users/kollah/OneDrive - Rensselaer Polytechnic Institute/Documents/rex/proj/src/main.jsx: Cannot find module '/mnt/c/users/kollah/OneDrive - Rensselaer Polytechnic Institute/Documents/rex/proj/node_modules/@jridgewell/gen-mapping/dist/gen-mapping.umd.js' /mnt/c/users/kollah/OneDrive - Rensselaer Polytechnic Institute/Documents/rex/proj/src/main.jsx

--- proj/vite.config.js
+++ proj/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // resolve: {
-  //   alias: {
-  //     '@jridgewell/gen-mapping': '@jridgewell/gen-mapping/src/gen-mapping.ts',
-  //   },
-  // },
 })
