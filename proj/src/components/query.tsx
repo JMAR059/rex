c17c2bff5508a073e13e8a6ce0ee10e5f6012edf
root <root@LAPTOP-LEB504MH.localdomain>
2025-10-04 23:24:18 -0400

    Updated css and made the button work

--- proj/src/components/query.tsx
+++ proj/src/components/query.tsx
@@ -1,15 +1,39 @@
+import { useState } from 'react';
+
 interface NavbarProps {
+  replaceResult: (resp: {result: string}) => void;
+  addToHistory: (query: string) => void;
 }
 
-const QueryBody: React.FC<NavbarProps> = ({ 
-}) => {
+const executeQuery = (query: string) => {
+  //Right now this just echos back the query, in the future it should call some api
+  return {"result": query};
+}
+
+const handleOnClick = (query: string, replaceResult: NavbarProps['replaceResult'], addToHistory: NavbarProps['addToHistory']) => {
+  //Ok so we want to execute the query, then receive the response 
+  const resp = executeQuery(query);
+  // and store it in the results
+  replaceResult(resp);
+  //We then want to save the query as a new element in the left bar, the history
+  addToHistory(query);
+};
+
+const QueryBody: React.FC<NavbarProps> = ({ replaceResult, addToHistory }) => {
+  const [query, setQuery] = useState<string>('');
   return (
-    <div>
-        <textarea>
+    <div className="flex flex-col">
+        <textarea
+        value={query}
+        onChange={(e) => setQuery(e.target.value)} 
+        className="w-full h-36 p-2 border rounded-md">
             Input here
         </textarea>
-        <button>
-            
+        <button 
+        onClick={() => handleOnClick(QueryBody, replaceResult, addToHistory)}
+        type="button"
+        className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md self-end hover:bg-blue-700">
+            Submit
         </button>
     </div>
   );
